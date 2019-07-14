470bc542beb07a73bd35adb15af75d167987cc71
freeman <freeman23@bigmir.net>
2019-07-14 16:59:57 +0300

    Updates

---
+++ precache-manifest.835d9308b6c9aea2e934a1216e1928e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace0f2e7fb7c4b2186e321aa5c338a8c",
+    "url": "/react-recipes-project/index.html"
+  },
+  {
+    "revision": "cdea35e37d0e9dec1862",
+    "url": "/react-recipes-project/static/css/main.b8129ee8.chunk.css"
+  },
+  {
+    "revision": "ddd4e623390ec3facaf3",
+    "url": "/react-recipes-project/static/js/2.77b90360.chunk.js"
+  },
+  {
+    "revision": "cdea35e37d0e9dec1862",
+    "url": "/react-recipes-project/static/js/main.3f23af5f.chunk.js"
+  },
+  {
+    "revision": "bf8cf8699befdd71b951",
+    "url": "/react-recipes-project/static/js/runtime~main.f9ab64e3.js"
+  }
+]);
